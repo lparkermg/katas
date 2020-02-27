2e81349a61512c8c844946b4113cb6637502e0ee
Luke Parker <lparkermg@outlook.com>
2020-02-27 13:19:12 +0000

    Refactored tests into one test with test cases.

--- src/Katas/Bowling.Tests/BowlingTests.cs
+++ src/Katas/Bowling.Tests/BowlingTests.cs
@@ -3,15 +3,18 @@ using NUnit.Framework;
 
 namespace Bowling.Tests
 {
+    [TestFixture]
     public class BowlingTests
     {
         private readonly Calculator _scoringCalculator = new Calculator();
-
-        [Test]
-        public void BowlingCalculator_WhenGivenASetOfZeroScores_ShouldReturnZero()
+        
+        [TestCase("00 00 00 00 00 00 00 00 00 000", 0)]
+        [TestCase("11 11 11 11 11 11 11 11 11 111", 21)]
+        [TestCase("12 12 12 12 12 12 12 12 12 121", 31)]
+        public void BowlingCalculator_Given10FramesWithCorrectNumbers_ShouldReturnExpectedResult(string frames, int expectedScore)
         {
-            var score = _scoringCalculator.Calculate("00 00 00 00 00 00 00 00 00 000");
-            Assert.AreEqual(0, score);
+            var score = _scoringCalculator.Calculate(frames);
+            Assert.AreEqual(expectedScore, score);
         }
     }
 }
