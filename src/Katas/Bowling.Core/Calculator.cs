d0e1d9ab89ebcd359bfb06d270e7121f649fcc6e
Luke Parker <lparkermg@outlook.com>
2020-02-27 13:49:31 +0000

    Added implementation for testcases where score is all numbers.

--- src/Katas/Bowling.Core/Calculator.cs
+++ src/Katas/Bowling.Core/Calculator.cs
@@ -1,12 +1,18 @@
-﻿using System;
-
-namespace Bowling.Core
+﻿namespace Bowling.Core
 {
     public sealed class Calculator
     {
         public int Calculate(string results)
-        {
-            return results == "11 11 11 11 11 11 11 11 11 111" ? 21 : 0;
+        { 
+            results = results.Replace(" ", string.Empty);
+
+            var totalScore = 0;
+            foreach (var score in results)
+            {
+                totalScore += int.Parse(score.ToString());
+            }
+
+            return totalScore;
         }
     }
 }
