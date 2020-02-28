453e8e65e82b99f2dadd9a81e791e1c59b5d44dd
Luke Parker <lparkermg@outlook.com>
2020-02-28 11:23:12 +0000

    Corrected framing in tests and added incomplete handling + spare support

--- src/Katas/Bowling.Tests/BowlingTests.cs
+++ src/Katas/Bowling.Tests/BowlingTests.cs
@@ -7,10 +7,9 @@ namespace Bowling.Tests
     public class BowlingTests
     {
         private readonly Calculator _scoringCalculator = new Calculator();
-        
-        [TestCase("00 00 00 00 00 00 00 00 00 000", 0)]
-        [TestCase("11 11 11 11 11 11 11 11 11 111", 21)]
-        [TestCase("12 12 12 12 12 12 12 12 12 121", 31)]
+        [TestCase("00 00 00 00 00 00 00 00 00 00", 0)]
+        [TestCase("11 11 11 11 11 11 11 11 11 11", 20)]
+        [TestCase("12 12 12 12 12 12 12 12 12 12", 30)]
         public void BowlingCalculator_Given10FramesWithCorrectNumbers_ShouldReturnExpectedResult(string frames, int expectedScore)
         {
             var score = _scoringCalculator.Calculate(frames);
@@ -27,14 +26,28 @@ namespace Bowling.Tests
         [Test]
         public void BowlingCalculator_GivenAMixOfStrikesAndNumberScores_ShouldReturnTheCorrectResult()
         {
-            var score = _scoringCalculator.Calculate("12 x 81 42 x 23 17 32 21 123");
-            Assert.AreEqual(79, score);
+            var score = _scoringCalculator.Calculate("12 x 81 42 x 23 17 32 21 12");
+            Assert.AreEqual(76, score);
         }
 
         [Test]
         public void BowlingCalculator_GivenUpperCaseStikes_ShouldReturnTheCorrectResult()
         {
-            var score = _scoringCalculator.Calculate("12 X 81 42 X 23 17 32 21 123");
+            var score = _scoringCalculator.Calculate("12 X 81 42 X 23 17 32 21 12");
+            Assert.AreEqual(76, score);
+        }
+
+        [Test]
+        public void BowlingCalculator_GivenIncompleteGame_ShouldReturnTheCurrentScore()
+        {
+            var score = _scoringCalculator.Calculate("11 12");
+            Assert.AreEqual(5, score);
+        }
+
+        [Test]
+        public void BowlingCalculator_GivenASpare_ShouldReturnTheCorrectResult()
+        {
+            var score = _scoringCalculator.Calculate("12 X 81 4/ 52 15 24 32 11 43");
             Assert.AreEqual(79, score);
         }
     }
