b7163d389a8e1bb6da1eb97f2da7a9c650f35581
Luke Parker <lparkermg@outlook.com>
2021-02-28 12:40:10 +0000

    Banking Kata (#1)
    * Implemented Account + Tests
    
    * Added to program.
    
    * Updated readme.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # katas
 Repo for all the Katas done.
+
+- Banking Kata ([https://kata-log.rocks/banking-kata](https://kata-log.rocks/banking-kata))
---
+++ src/banking-kata/BankingKata/Program.cs
@@ -0,0 +1,22 @@
+﻿using BankingKata.Entities;
+using System;
+
+namespace BankingKata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Accounts!");
+
+            var account = new Account();
+
+            account.Deposit(500);
+            account.Withdraw(50);
+            account.Withdraw(425);
+            account.Withdraw(100);
+
+            Console.Write(account.PrintStatement());
+        }
+    }
+}
